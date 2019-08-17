da94facc4146b77f90a467821a53bf821471d13b
Azriel Hoh <azriel91@gmail.com>
2019-08-17 15:51:32 +1200

    Updated documentation and `README.md`.

--- src/lib.rs
+++ src/lib.rs
@@ -258,11 +258,12 @@
 //!
 //!     ```rust,edition2018
 //!     use proc_macro_roids::FieldExt;
-//!     use syn::{parse_quote, Fields, FieldsNamed};
+//!     use proc_macro2::Span;
+//!     use syn::{parse_quote, Ident, Fields, FieldsNamed, Lit, LitStr, Meta, MetaNameValue};
 //!
 //!     # fn main() {
 //!     let fields_named: FieldsNamed = parse_quote! {{
-//!         #[my_derive(tag_name)]
+//!         #[my_derive(tag_name(param = "value"))]
 //!         pub name: PhantomData<T>,
 //!     }};
 //!     let fields = Fields::from(fields_named);
@@ -271,6 +272,14 @@
 //!     assert_eq!(field.type_name(), "PhantomData");
 //!     assert!(field.is_phantom_data());
 //!     assert!(field.contains_tag("my_derive", "tag_name"));
+//!     assert_eq!(
+//!         field.tag_parameter("my_derive", "tag_name").expect("Expected parameter to exist."),
+//!         Meta::NameValue(MetaNameValue {
+//!             ident: Ident::new("param", Span::call_site()),
+//!             eq_token: Default::default(),
+//!             lit: Lit::Str(LitStr::new("value", Span::call_site())),
+//!         }),
+//!     );
 //!     # }
 //!     ```
 
